4d62e53797efed5007aa82814249cfdc4dca03f2
pekka1112 <thanhphatle20032017@gmail.com>
2023-12-04 19:10:09 +0700

    prepare_to_connect_db

---
+++ src/main/java/database/KhachHangDAO.java
@@ -0,0 +1,42 @@
+package database;
+
+import model.KhachHang;
+
+import java.util.ArrayList;
+
+public class KhachHangDAO implements DAOInterface<KhachHang>{
+    @Override
+    public ArrayList<KhachHang> selectAll() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<KhachHang> selectById(KhachHang object) {
+        return null;
+    }
+
+    @Override
+    public int insert(KhachHang obj) {
+        return 0;
+    }
+
+    @Override
+    public int insertAll(ArrayList<KhachHang> arrayList) {
+        return 0;
+    }
+
+    @Override
+    public int delete(KhachHang obj) {
+        return 0;
+    }
+
+    @Override
+    public int deleteAll(ArrayList<KhachHang> arrayList) {
+        return 0;
+    }
+
+    @Override
+    public int update(KhachHang obj) {
+        return 0;
+    }
+}
