13722b4d52fa9d0581f706ee8286bb7b65f231d6
pekka1112 <phatmapvl2017@gmail.com>
2024-08-28 18:53:31 +0700

    alt : jdbc

--- src/main/java/database/JDBCUtil.java
+++ src/main/java/database/JDBCUtil.java
@@ -6,35 +6,13 @@ import java.sql.SQLException;
 
 
 public class JDBCUtil {
-//    public static Connection getConnection() {
-//        Connection c = null;
-//
-//        try {
-//            // Đăng ký MySQL Driver với DriverManager
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//
-//            // Các thông số
-//            String url = "jdbc:mySQL://localhost:3306/movie_ticket_db";
-//            String username = "root";
-//            String password = "";
-//
-//            // Tạo kết nối
-//            c = DriverManager.getConnection(url, username, password);
-//
-//        } catch (SQLException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//
-//        return c;
-//    }
     private static String url = "jdbc:mysql://localhost:3306/movie_ticket?useUnicode=true&characterEncoding=utf8";
     private static String username = "root";
     private static String password = "";
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("successfully");
+            System.out.println("Connection database successfully");
             return DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
